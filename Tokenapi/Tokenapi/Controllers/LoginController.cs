a88374e6af8752645d2f495311d4199c1c228d64
yigithan <yigithankaraman@hotmail.com>
2025-01-24 11:48:54 +0300

    addedTokenApiCodes

---
+++ Tokenapi/Tokenapi/Controllers/LoginController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using Tokenapi.Model;
+
+namespace Tokenapi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Post([FromBody]LoginDTO credentials)
+        {
+            var user = UserList.SingleOrDefault(x => x.Username == credentials.Username && x.Password == credentials.Password);
+            if (user == null) return Ok(new { Hata = "Gerçersiz Kullanıcı veya Şifre" });
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_config["Application:Secret"]);
+            
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Audience = "SomeCustomApp",
+                Issuer = "yk.api.demo",
+                
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, credentials.Username),
+                    //new Claim(ClaimTypes.HomePhone,"+90 555 123 45 67"),
+                    new Claim("Password", credentials.Password)
+                }),
+                NotBefore = DateTime.Now,
+                Expires = DateTime.Now.AddSeconds(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new { Token = tokenString });
+        }
+
+        private List<LoginDTO> UserList = new List<LoginDTO>
+        {
+            new LoginDTO { Username = "yigit", Password= "123456" },
+            new LoginDTO { Username = "yigit1", Password= "12345" },
+            new LoginDTO { Username = "yigit2", Password= "1234" },
+            new LoginDTO { Username = "yigit3", Password= "123" },
+        };
+    }
+}
---
+++ Tokenapi/Tokenapi/Controllers/WritetextController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Tokenapi.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WritetextController : ControllerBase
+    {
+        // GET: api/Writetext
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Writetext/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Writetext
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Writetext/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
